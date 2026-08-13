c4b2e8fd57c433670cf3cd40f48afb30d14dc274
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-13 14:25:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const port = process.env.PORT || 3000;
 
 app.use('/static', express.static('public'));
 app.use(cors({
-    origin: "http://localhost:3000",   
-    credentials: true                    
-}));        
+    origin: ["http://localhost:3000", "https://student-portal-nextjs-swart.vercel.app"],
+    credentials: true
+}));       
 app.use(express.json()); 
 app.use(cookieParser());
 
