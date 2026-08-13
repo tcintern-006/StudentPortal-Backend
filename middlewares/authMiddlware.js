bf126be457ad0553cea2d73c22fbf5d6feea30f4
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-13 15:00:38 +0500

    Update authMiddlware.js

--- middlewares/authMiddlware.js
+++ middlewares/authMiddlware.js
@@ -1,29 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-const protect = (req , res , next)=>{
+const protect = (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-    const token = req.cookies.token;
-    
-    if(!token){
-       return  res.status(401).json({message: 'Not authincated'})
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Not authenticated" });
     }
+
+    const token = authHeader.split(" ")[1];
 
     try {
-
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        )
-
-        req.user = decoded
-        next()
-        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
     } catch (error) {
-        console.log(error)
-       res.status(401).json({ message: 'Invalid or expired token' });
+        res.status(401).json({ message: "Invalid or expired token" });
     }
-
 }
-
 
 module.exports = protect;
