3a55b4c23c4e47ada4a8029d8490b83e1f1d1ee1
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-16 20:22:21 +0500

    Update instructorRoutes.js

--- routes/instructorRoutes.js
+++ routes/instructorRoutes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const { getallInstructors, addInstructors, updateInstructors, deleteInstructors } = require('../controllers/instructorsController');
 const { validateInstructor } = require('../middlewares/validateData');
 const adminAccess = require("../middlewares/adminAccess");
+const protect = require("../middlewares/authMiddlware");
 const router = express.Router()
 
 
 router.get('/instructors', getallInstructors)
 
-router.post('/instructors' , validateInstructor , adminAccess ,addInstructors)
+router.post('/instructors' , validateInstructor ,protect, adminAccess ,addInstructors)
 
-router.put('/instructors/:id', adminAccess, updateInstructors)
+router.put('/instructors/:id',protect, adminAccess, updateInstructors)
 
-router.delete('/instructors/:id', adminAccess,deleteInstructors)
+router.delete('/instructors/:id',protect, adminAccess,deleteInstructors)
 
 
 module.exports = router;
